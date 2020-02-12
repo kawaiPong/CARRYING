f9616c0eee808cc2533f5bba82842808bb89a640
yeLyn36 <s2018w11@e-mirim.hs.kr>
2020-02-13 00:47:35 +0900

    add firebase user Authentication

---
+++ app/src/main/java/kr/hs/emirim/lyn/carrying/SignInActivity.java
@@ -0,0 +1,65 @@
+package kr.hs.emirim.lyn.carrying;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignInActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+
+//        init();
+//        buttonListener();
+
+        auth = FirebaseAuth.getInstance();
+
+//        sign_in_btn.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View view) {
+//
+//                String email = sign_in_id.getText().toString().trim();
+//                String pw = sign_in_pw.getText().toString().trim();
+//
+//                firebaseAuth.signInWithEmailAndPassword(email, pw)
+//                        .addOnCompleteListener(LoginActivity.this, task -> {
+//
+//                            if (task.isSuccessful()) {
+//                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//                                startActivity(intent);
+//                            } else {
+//                                Toast.makeText(LoginActivity.this, "로그인 오류", Toast.LENGTH_SHORT).show();
+//                            }
+//
+//                        });
+//            }
+//
+//        });
+    }
+//
+//    private void init() {
+//
+//        sign_in_btn = findViewById(R.id.sign_in_btn);
+//        register_Text = findViewById(R.id.register_Text);
+//        sign_in_id = findViewById(R.id.sign_in_id);
+//        sign_in_pw = findViewById(R.id.sign_in_pw);
+//
+//    }
+//
+//    private void buttonListener() {
+//        register_Text.setOnClickListener(view -> {
+//            startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+//        });
+//    }
+}
