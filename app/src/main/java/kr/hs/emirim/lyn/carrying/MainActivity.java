8ac099300048a315348bfc997e096b6f59df4487
yeLyn36 <s2018w11@e-mirim.hs.kr>
2020-02-14 06:22:07 +0900

    add signIn by 4 ways (email, anonymous, facebook, google)

--- app/src/main/java/kr/hs/emirim/lyn/carrying/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/lyn/carrying/MainActivity.java
@@ -2,19 +2,47 @@ package kr.hs.emirim.lyn.carrying;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import java.util.List;
+
+import kr.hs.emirim.lyn.carrying.Login.SignInActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     public final static String TAG = "MainActivity";
 
+    private FirebaseAuth auth;
+    private FirebaseUser user;
+
+    Button signOut_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
+        signOut_btn = (Button)findViewById(R.id.signOut);
+        signOut_btn.setOnClickListener(view -> {
+            auth.signOut();
+            Log.d(TAG, "로그아웃 버튼");
+            Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+            startActivity(intent);
+        });
+
+        //익명 인증일 경우 user.getDisplayName == NULL;
+        // Log.d(TAG, user.getUid()); 페이스북 로그인에 에러
+
         initLoadDB();
     }
 
