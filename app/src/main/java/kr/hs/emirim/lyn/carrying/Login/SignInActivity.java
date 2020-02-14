4fa39f59cbc89291c0edb2bb55beaddabba7e9e9
ddochiii <s2018s29@e-mirim.hs.kr>
2020-02-14 16:55:44 +0900

    Transferred to code 'signActivity' in 'mainActivity'.

--- app/src/main/java/kr/hs/emirim/lyn/carrying/Login/SignInActivity.java
+++ app/src/main/java/kr/hs/emirim/lyn/carrying/Login/SignInActivity.java
@@ -5,22 +5,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.List;
+
+import kr.hs.emirim.lyn.carrying.DataAdapter;
 import kr.hs.emirim.lyn.carrying.R;
+import kr.hs.emirim.lyn.carrying.User;
 
 public class SignInActivity extends BaseActivity {
 
     private FirebaseAuth auth;
+
+    Button SignIn_email;
+    Button SignIn_pw;
+    Button SignIn_btn;
+    Button checkPW_btn;
+    Button SignUp_btn;
+    Button fb_btn;
+    Button tt_btn;
+    Button gg_btn;
+    Button anonymous;
+    Button emailText;
+    Button pwText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
+        initLoadDB();
+        this.InitializeView();
+        this.SetListener();
 
 //        init();
 //        buttonListener();
@@ -83,4 +103,111 @@ public class SignInActivity extends BaseActivity {
 //            startActivity(new Intent(SignInActivity.this, RegisterActivity.class));
 //        });
 //    }
+
+
+
+    private void SetListener() {
+        SignIn_btn=(Button)(findViewById(R.id.Signin));
+        SignIn_email=(Button)(findViewById(R.id.signin_email));;
+        SignIn_pw=(Button)(findViewById(R.id.signIn_pw));;
+        checkPW_btn=(Button)(findViewById(R.id.check));;
+        SignUp_btn=(Button)(findViewById(R.id.signup));;
+        fb_btn=(Button)(findViewById(R.id.fb));;
+        tt_btn=(Button)(findViewById(R.id.tt));;
+        gg_btn=(Button)(findViewById(R.id.gg));;
+        anonymous=(Button)(findViewById(R.id.anonymous));;
+
+    }
+
+    private void InitializeView() {
+        SignIn_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(getApplicationContext(), "테스트테스트:::"+userList.get(0).getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        SignIn_email.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(getApplicationContext(), "테스트테스트:::"+userList.get(0).getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        SignIn_pw.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(getApplicationContext(), "테스트테스트:::"+userList.get(0).getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        checkPW_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(getApplicationContext(), "테스트테스트:::"+userList.get(0).getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        fb_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(getApplicationContext(), "테스트테스트:::"+userList.get(0).getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        tt_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(getApplicationContext(), "테스트테스트:::"+userList.get(0).getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        gg_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(getApplicationContext(), "테스트테스트:::"+userList.get(0).getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        anonymous.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(getApplicationContext(), "테스트테스트:::"+userList.get(0).getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        SignUp_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(getApplicationContext(), "테스트테스트:::"+userList.get(0).getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+
+    public List<User> userList ;
+
+    private void initLoadDB() {
+
+        DataAdapter mDbHelper = new DataAdapter(getApplicationContext());
+        mDbHelper.createDatabase();
+        mDbHelper.open();
+
+        // db에 있는 값들을 model을 적용해서 넣는다.
+        userList = mDbHelper.getTableData();
+        Toast.makeText(getApplicationContext(), "테스트테스트:::"+userList.get(0).getName(), Toast.LENGTH_LONG).show();
+        mDbHelper.close();
+    }
+
+
 }
