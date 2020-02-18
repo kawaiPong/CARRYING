35a26b1e7955c85d4e508163c1b0c28a2d9039cb
LHyunSoo <s2018w36@e-mirim.hs.kr>
2020-02-19 00:44:25 +0900

    splash screen

---
+++ app/src/main/java/kr/hs/emirim/lyn/carrying/SplashActivity.java
@@ -0,0 +1,30 @@
+package kr.hs.emirim.lyn.carrying;
+
+import androidx.appcompat.app.AppCompatActivity;
+import kr.hs.emirim.lyn.carrying.Login.SignInActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, SignInActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, 1800);
+    }
+}
