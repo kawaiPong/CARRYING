fd7c3a4cf44d8d59f17cbef78353bc0ce797b287
yeLyn36 <s2018w11@e-mirim.hs.kr>
2020-02-19 01:48:12 +0900

    Register member success

--- app/src/main/java/kr/hs/emirim/lyn/carrying/Login/RegisterActivity.java
+++ app/src/main/java/kr/hs/emirim/lyn/carrying/Login/RegisterActivity.java
@@ -2,6 +2,7 @@ package kr.hs.emirim.lyn.carrying.Login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,15 +13,34 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import kr.hs.emirim.lyn.carrying.R;
+import kr.hs.emirim.lyn.carrying.create_list;
 
 public class RegisterActivity extends BaseActivity implements AdapterView.OnItemSelectedListener {
 
+    private final static String TAG = "RegisterActivity";
+
     private FirebaseAuth auth;
+
     Spinner spinner;
     String[] item;
 
+    EditText et_name;
+    EditText et_email;
+    EditText et_password;
+    EditText et_checkPassword;
+    TextView loginBtn;
+    Button registerBtn;
+
+    String nickname;
+    String email;
+    String password;
+    String checkPassword;
+    int gender;
+
+    String uid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,21 +48,6 @@ public class RegisterActivity extends BaseActivity implements AdapterView.OnItem
         setContentView(R.layout.activity_register);
 
         auth = FirebaseAuth.getInstance();
-
-        String email = "ghddnjf2901@gmail.com".trim();
-        String pw = "dnjs290112sb*".trim();
-
-        auth.createUserWithEmailAndPassword(email, pw)
-                .addOnCompleteListener(RegisterActivity.this, task -> {
-                    if (task.isSuccessful()) {
-                        Intent intent = new Intent(RegisterActivity.this, SignInActivity.class);
-                        startActivity(intent);
-                        finish();
-                    } else {
-                        Toast.makeText(RegisterActivity.this, "등록 에러", Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-                });
 
         spinner = (Spinner) findViewById(R.id.spinner);
 
@@ -55,8 +60,6 @@ public class RegisterActivity extends BaseActivity implements AdapterView.OnItem
 
         spinner.setAdapter(adapter);
 
-//        buttonLister();
-
         Button back = (Button) findViewById(R.id.backbtn);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,40 +68,52 @@ public class RegisterActivity extends BaseActivity implements AdapterView.OnItem
             }
         });
 
-        TextView login=(TextView)findViewById(R.id.textView1);
-        login.setOnClickListener(new View.OnClickListener() {
+        loginBtn=(TextView)findViewById(R.id.textView1);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
-
             }
         });
 
-        Button joinbtn = (Button) findViewById(R.id.joinbtn);
-        joinbtn.setOnClickListener(new View.OnClickListener() {
+        registerBtn = (Button) findViewById(R.id.joinbtn);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText NickNameE = (EditText) findViewById(R.id.et_name);
-                EditText EmailE = (EditText) findViewById(R.id.et_eamil);
-                EditText PasswordE = (EditText) findViewById(R.id.et_password);
-                EditText CheckPasswordE = (EditText) findViewById(R.id.et_passwordre);
+                et_name = (EditText) findViewById(R.id.et_name);
+                et_email = (EditText) findViewById(R.id.et_eamil);
+                et_password = (EditText) findViewById(R.id.et_password);
+                et_checkPassword = (EditText) findViewById(R.id.et_passwordre);
 
+                nickname = et_name.getText().toString().trim();
+                email = et_email.getText().toString().trim();
+                password = et_password.getText().toString().trim();
+                checkPassword = et_checkPassword.getText().toString().trim();
 
-                String NickName = NickNameE.getText().toString().trim();
-                String Email = EmailE.getText().toString().trim();
-                String Password = PasswordE.getText().toString().trim();
-                String CheckPassword = CheckPasswordE.getText().toString().trim();
+                Log.d(TAG, nickname + ", " + email + ", " + password + ", " + checkPassword + ", " + spinner.isSelected());
 
-                if ((NickName.length() == 0) ||
-                        (Email.length() == 0) ||
-                        (Password.length() == 0) ||
-                        (CheckPassword.length() == 0)) {
+                if ((nickname.length() == 0) ||
+                        (email.length() == 0) ||
+                        (password.length() == 0) ||
+                        (checkPassword.length() == 0) ||
+                        !(spinner.isSelected())) {
                     Toast.makeText(getApplicationContext(), "모든 항목이 채워져있는지 확인해주세요", Toast.LENGTH_LONG).show();
 
                 } else {
-                    if (Password.equals(CheckPassword)) {
-                        Intent intent = new Intent(RegisterActivity.this, SignInActivity.class);
-                        startActivity(intent);
+                    if (password.equals(checkPassword)) {
+                        auth.createUserWithEmailAndPassword(email, password)
+                                .addOnCompleteListener(RegisterActivity.this, task -> {
+                                    if (task.isSuccessful()) {
+                                        uid = auth.getCurrentUser().getUid();
+                                        Log.d(TAG + ": UID", uid);
+                                        Intent intent = new Intent(RegisterActivity.this, create_list.class);
+                                        startActivity(intent);
+                                        finish();
+                                    } else {
+                                        Toast.makeText(RegisterActivity.this, "등록 에러", Toast.LENGTH_SHORT).show();
+                                        return;
+                                    }
+                                });
                     }
                     else{
                         Toast.makeText(getApplicationContext(), "비밀번호 확인이 일치하지 않습니다.", Toast.LENGTH_LONG).show();
@@ -111,7 +126,8 @@ public class RegisterActivity extends BaseActivity implements AdapterView.OnItem
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-
+        gender = i;
+        spinner.setSelected(true);
     }
 
     @Override
