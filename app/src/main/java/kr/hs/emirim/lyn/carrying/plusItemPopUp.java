de68a8e583fdeaba31d000989ccc8fa68683dcfc
소원 박 <s2018s29@e-mirim.hs.kr>
2020-05-14 01:56:53 +0900

    아이템 추가하는 팝업 style, manifest 수정 class에서 서버연결부분 해야함

--- app/src/main/java/kr/hs/emirim/lyn/carrying/plusItemPopUp.java
+++ app/src/main/java/kr/hs/emirim/lyn/carrying/plusItemPopUp.java
@@ -3,6 +3,10 @@ package kr.hs.emirim.lyn.carrying;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class plusItemPopUp extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class plusItemPopUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plus_item_pop_up);
+
+        EditText plus_item_text=(EditText) findViewById(R.id.plus_item_popup);//추가하는 물품 텍스트
+        Button submit=(Button)findViewById(R.id.check_popup);//확인버튼
+
+
+
+        submit.setOnClickListener(new View.OnClickListener() {//삭제하는 retrofit
+            @Override
+            public void onClick(View v) {
+                plus_item_text.getText().toString();
+                Log.d("sowon plus item popUp",plus_item_text.getText().toString());
+
+            }
+        });
+
+
     }
 }
